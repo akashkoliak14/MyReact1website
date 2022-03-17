dc59a922a2f8a18cee3d35e45f8c7e4897418869
akashkoliak14 <akashkoli100@gmail.com>
2022-03-17 21:18:57 +0530

    "hedc"

--- src/home.jsx
+++ src/home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import img1 from './image/veg1.jpg'
 
 function home() {
     
     return (
 <div>
 
-
+<img src={img1} className="w-100"></img>
     </div>
 )
 }
