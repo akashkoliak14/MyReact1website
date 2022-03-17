cff9804e958f6f8720102a81f0ec204cf68c1883
akashkoliak14 <akashkoli100@gmail.com>
2022-03-17 19:43:56 +0530

    "head"

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <header> 
      
       {header()}
-      {home()}
+       {home()}
     </header>
 </>
   );
--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,6 @@
-
+import React from 'react';
 import './App.css';
+
 
 
 function home() {
@@ -7,14 +8,10 @@ function home() {
     return (
 <div>
  
-    <img
-      className="d-block w-100"
-      src=".image\fernando-alvarez-rodriguez-M7GddPqJowg-unsplash.jpg"
-      alt="no image found"
-    />
+<div>
+    <div>helow it is Home</div>
     
-    
-
+</div>
   
    
     </div>
